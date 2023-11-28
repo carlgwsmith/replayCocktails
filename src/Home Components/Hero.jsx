88c47135f21947d62ef3f8569e998ad933773a3e
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2023-11-27 21:05:14 -0600

    Update Hero.jsx

--- src/Home Components/Hero.jsx
+++ src/Home Components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero(){
     <div className="grid grid-cols-12 h-[460px] heroBg">
         <div className="xs:col-span-10 xs:px-[60px] md:col-span-8 clipBg">
             <div className="alignMiddle">
-            <h2 className="xs:text-[32px] xs:leading-[32px] sm:text-[46px] sm:leading-[46px] md:text-[56px] md:leading-[54px]">A COCKTAIL<br/> <span className="font-bold">AS BOLD AS YOU</span></h2>
+            <h2 className="xs:text-[32px] xs:leading-[32px] sm:text-[46px] sm:leading-[46px] md:text-[68px] md:leading-[58px]">A COCKTAIL<br/> <span className="font-bold">AS BOLD AS YOU</span></h2>
             </div>
             <button className="py-3 px-6 rounded-md border-1 border-snow uppercase hover:text-[#646cff]">Find near you</button>
         </div>
