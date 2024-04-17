4722ac291b17266d3759f9dcf0c2dc60c86c5571
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2024-04-16 21:57:35 -0500

    Update footer.jsx

--- src/Global Components/footer.jsx
+++ src/Global Components/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer(){
         <div className="footer">
         <div className="left-0 bottom-0 w-full bg-gunmetal text-snow">
             <div className='text-center w-full p-4 text-sm font-semibold text-snow'>
-                &copy; Replay Cocktails 2023 | Always Enjoy Responsibly.  Do not share this content with Minors.  All rights reserved.
+                &copy; Replay Cocktails 2024 | Always Enjoy Responsibly.  Do not share this content with Minors.  All rights reserved.
             </div>
         </div>
         </div>
