1c20cf1000c13c02ebfc16eb854896a109cc5d37
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2024-04-17 22:29:18 -0500

    mobile

--- src/Pages/WhereToBuy.jsx
+++ src/Pages/WhereToBuy.jsx
@@ -2,16 +2,16 @@ import LocationMap from "../Map/locationMap"
 import Locations from "../Map/locations"
 export default function WhereToBuy(){
     return(<>
-    <div className="grid grid-cols-12 mx-[60px] my-[40px] ">
+    <div className="grid grid-cols-12 mx-[60px] my-[40px] whereToBuy">
         <div className="col-span-12">
         <h3 className="text-center xs:text-[28px] sm:text-[32px] md:text-[42px] mb-8 uppercase text-gunmetal font-bold">Where to buy</h3>
         </div>
-        <div className="col-span-6">
+        <div className="col-span-12">
         <LocationMap/>
         </div>
-        <div className="col-span-6">
+        {/* <div className="col-span-6">
             <Locations/>
-        </div>
+        </div> */}
     </div>
     </>)
 }
