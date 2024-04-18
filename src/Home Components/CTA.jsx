f419fdc653ec2d3fc5d0b2fae3792d06d9d5dcb6
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2024-04-17 21:29:42 -0500

    mobile fixes

--- src/Home Components/CTA.jsx
+++ src/Home Components/CTA.jsx
@@ -6,7 +6,7 @@ export default function CallToAction (){
     <div className="grid grid-cols-12 py-12 cta-home">
         <div className="col-span-12 pt-8">
         <h2 className="text-center xs:text-[28px] sm:text-[32px] md:text-[42px] uppercase text-gunmetal font-bold">4 Inspiring Flavors</h2>
-        <p className="text-gunmetal m-auto text-center px-40 py-8  xs:text-[16px] sm:text-[18px] md:text-[22px]">Our drinks were born in the Spring of 2020, when live music stopped.   
+        <p className="text-gunmetal m-auto text-center xs:px-[35px] md:px-40 py-8  xs:text-[16px] sm:text-[18px] md:text-[22px]">Our drinks were born in the Spring of 2020, when live music stopped.   
 We looked past the empty stages and dreamt up an idea to share the feeling of seeing
 your favorite band play your favorite song.
 Our drink names honor our favorite local and regional bands.</p>
