523f95ac195723562bab045f3c78a92519294858
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2024-04-17 22:39:19 -0500

    mobile stuff

--- src/Global Components/cocktails.jsx
+++ src/Global Components/cocktails.jsx
@@ -5,7 +5,7 @@ import NancyBoy from '../Pages/NancyBoy'
 import Vibralux from '../Pages/Vibralux'
 export default function Cocktails (){
     return(
-    <div className="grid grid-cols-12 mx-[60px] mt-[40px] gap-8">
+    <div className="grid grid-cols-12 xs:mx-[30px] xs:mt-[20px] md:mx-[60px] md:mt-[40px] gap-8">
         <div className="xs:col-span-12 sm:col-span-6 md:col-span-3">
             <CocktailCard img="/Images/artwork/h300/BELTANE_FRONT.jpg" img2="/Images/artwork/h300/BELTANE_SIDE.jpg" name="Beltane" desc="Cucumber infused Vodka with Gin, Lime, and Mint. Expertly Balanced." abv="8% ABV" modalContent={<Beltane/>}/>
         </div>
