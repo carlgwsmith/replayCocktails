aebdb3e043599e34c7184e8d960e79499639c82e
Carl Smith <48308402+carlgwsmith@users.noreply.github.com>
2024-06-20 20:57:34 -0500

    Update Vibralux.jsx

--- src/Pages/Vibralux.jsx
+++ src/Pages/Vibralux.jsx
@@ -6,11 +6,6 @@ Vibralux, the most dazzling drink to hit the scene since platform boots.</h4>
         <p className="mb-10">This ain&#39;t your grandma&#39;s gin and tonic. Vibralux is a symphony of flavor, a rockin&#39; fusion of
 iconic London Dry Gin, bursting with the juicy sweetness of fresh strawberries, the herbal magic
 of basil, and a zest of lime that cuts through like a killer guitar solo.</p>
-<p className="mb-10">
-Forget the cocktail shaker, baby! Vibralux comes pre-mixed and ready to rock your tastebuds,
-wherever the party takes you. Whether you&#39;re backstage at a concert, lighting up the dance floor,
-or posing for the paparazzi, Vibralux is the liquid fuel for your next legendary night.</p>
-
 <p>So crank up the volume, unleash your inner rockstar, and experience the vibrant taste of Vibralux!</p></>
     )
 }
